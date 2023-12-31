5c8755be85e7d23609c611c63675b752412d26b9
Daniel Holt <holt.daniel.b@gmail.com>
2023-12-31 13:18:33 -0500

    server running

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const characterRouter = require('./router/character-router');
+
+// Create server
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+//middleware
+app.use(express.json());
+app.use(express.static('server/public'));
+
+//express routes
+app.use('/characters', characterRouter);
+
+// Start server
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
